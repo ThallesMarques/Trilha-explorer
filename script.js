8d8a70d7cf9145e2cf31fe36dd3760df2ab9b03a
m304555 <thalles.marques@capgemini.com>
2023-01-23 08:54:42 -0300

    add js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const button = document.querySelector("header button")
 button.addEventListener("click", add)
 form.addEventListener("change", save)
 
-function add() { 
+function add() {
   //const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
   const dayExists = nwlSetup.dayExists(today)
 
